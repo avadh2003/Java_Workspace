e0896a809d1153634738b86523b6439b701e2678
avadh2003 <114382325+avadh2003@users.noreply.github.com>
2023-02-15 15:40:23 +0530

    Add files via upload

---
+++ task/13-2/C1.java
@@ -0,0 +1,9 @@
+package P1;
+
+public class C1 
+{
+	public void msg()
+	{
+		System.out.println("sending msg...");
+	}
+}
---
+++ task/13-2/C2.java
@@ -0,0 +1,13 @@
+package P2;
+
+import P1.C1;
+
+public class C2 
+{
+	public static void main(String[] args) 
+	{
+		C1 c = new C1();
+		
+		c.msg();
+	}
+}
